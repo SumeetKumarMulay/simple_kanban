8994cdd57039d6fa488139f4c211b07b464ab8d2
SumeetMulay <sumeetkumar.mulay@gmail.com>
2025-04-07 13:32:12 +0900

    * Added splach screen * Added Launch screen

--- lib/screens/kanban_screens/completed_screen.dart
+++ lib/screens/kanban_screens/completed_screen.dart
@@ -1,5 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:simple_kanban/state/todo_bloc.dart';
+import 'package:simple_kanban/utilities/models/enums/enums.dart';
+import 'package:simple_kanban/utilities/widgets/kanban_view.dart';
 
 @RoutePage()
 class CompletedScreen extends StatefulWidget {
@@ -12,6 +16,23 @@ class CompletedScreen extends StatefulWidget {
 class _CompletedScreenState extends State<CompletedScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Completed Screen')));
+    return Scaffold(
+      body: BlocConsumer<TodoBloc, TodoStates>(
+        builder: (context, state) {
+          switch (state) {
+            case Initial(:final completeList):
+              return KanbanView(
+                data: completeList,
+                routedFrom: RoutedFrom.todoPage,
+              );
+            case Loading():
+              return Center(child: CircularProgressIndicator());
+            case _:
+              return Center(child: CircularProgressIndicator());
+          }
+        },
+        listener: (context, state) => {},
+      ),
+    );
   }
 }
